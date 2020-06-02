50c4643a5a131bd86fa5b09fc70fbc3862c5cd8e
Morgan Bartlett <mbartlet@uoregon.edu>
2020-06-02 16:11:17 -0700

    Finished RESTful Web Service that stores to-do list items

---
+++ client/javascripts/todo.js
@@ -0,0 +1,114 @@
+//jshint esversion: 6
+
+let controller = function() {
+
+  $.ajax({
+    url: "http://localhost:8888/todos",
+    method: "GET"
+  }).done((res) => {
+    //create new paragrah element
+    let pElem;
+    //console.log(res.todo[0]._id + " " + res.todo[0].data)
+    //for each todo item, append the content to the paragraph element
+    res.todos.forEach((todo) => {
+      pElem = $("<p>").html(todo.data)
+      $(".comments").append(pElem);
+    })
+  });
+
+  let addCommentFromInputBox = function() {
+    //Semmy uses "$" to name variables that will contain jQuery objects
+    let $new_todo, content;
+
+    if ($(".comment-input input").val() !== "") {
+      content = $(".comment-input input").val();
+      $new_todo = $("<p>").text(content);
+      $new_todo.hide();
+      $(".comments").append($new_todo);
+      $new_todo.fadeIn();
+      $(".comment-input input").val("");
+
+      //add comment to db
+      $.ajax({
+          method: "POST",
+          url: "http://localhost:8888/addtodo",
+          data: {
+            data: content
+          }
+        })
+        .done(function(msg) {
+          console.log("Data Saved: " + msg);
+        });
+    }
+  };
+
+  $(".comment-input button").on("click", function(event) {
+    addCommentFromInputBox();
+  });
+
+  $(".comment-input input").on("keypress", function(event) {
+    if (event.keyCode === 13) {
+      addCommentFromInputBox();
+    }
+  });
+};
+
+let deleteTodo = () => {
+  //delete a comment from db
+  let content = $("#deleteOne").val();
+  $.ajax({
+      method: "POST",
+      url: "http://localhost:8888/deletetodo/" + content
+    })
+    .done(function(msg) {
+      console.log("Comment deleted: " + msg);
+    });
+
+  window.location.reload();
+}
+
+let getTodo = () => {
+  //clear outDiv
+  $("#outDiv").html("");
+  let pElem;
+  //retrieve a comment from db
+  let content = $("#getOne").val();
+  $.ajax({
+      method: "GET",
+      url: "http://localhost:8888/gettodo/" + content
+    })
+    .done(function(msg) {
+      console.log("Todo retrieved: " + msg.message.data);
+      pElem = $("<p>").html("Todo Retrieved: " + msg.message.data)
+      $("#outDiv").append(pElem);
+    });
+
+  //window.location.reload();
+}
+
+/* let deleteAll = () => {
+  //delete all comments from db
+  //TBA
+  $.ajax({
+    method: "DELETE",
+    url: "http://localhost:8888/deleteall/"
+  })
+  .done(function(msg) {
+    console.log("List items deleted: " + msg);
+  });
+
+  //window.location.reload();
+}
+*/
+$(document).ready(() => {
+  let btn01, btn02, btn03;
+  //console.log("ready")
+  //select the delete button
+  btn03 = document.querySelectorAll('button')[3];
+  btn03.addEventListener('click', deleteAll);
+  btn02 = document.querySelectorAll('button')[2];
+  btn02.addEventListener('click', deleteTodo);
+  btn01 = document.querySelectorAll('button')[1];
+  btn01.addEventListener('click', getTodo);
+  controller();
+});
